5f0fc5b6bd51cda3d1fd6f5b05ce13d2f501a74d
kilati <kilati74@gmail.com>
2024-07-27 17:09:21 +0200

    main

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -15,7 +15,7 @@ export default function Error404() {
             height="128"
             alt="the Fresh logo: a sliced lemon dripping with juice"
           />
-          <h1 class="text-4xl font-bold">404 - Page not found</h1>
+          <h1 class="text-4xl font-bold">404 - Page not fikad</h1>
           <p class="my-4">
             The page you were looking for doesn't exist.
           </p>
